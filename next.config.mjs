a61f682ea5501dcee97e4d91aa0b53deea2177b8
taha <baghaztaha1.com>
2026-08-07 00:08:27 +0100

    fix image paths with basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // هذا السطر ضروري جداً لتصدير صفحات static
+  output: 'export',
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
+  images: {
+    unoptimized: true, // ضروري لمشاريع Next.js المصدّرة كـ Static
+  },
 };
 
 export default nextConfig;
-
